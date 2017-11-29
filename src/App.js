773ffe3e85e168b62230a76ba963aa04e8b97ed4
Cole Carroll <colecarroll2@gmail.com>
2017-11-29 13:42:33 -0700

    final version commit

--- src/App.js
+++ src/App.js
@@ -5,70 +5,6 @@ import MessageList from './components/MessageList'
 import Toolbar from './components/Toolbar'
 import ComposeForm from './components/ComposeForm'
 
-// const data =
-// [
-//   {
-//     "id": 1,
-//     "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
-//     "read": false,
-//     "starred": false,
-//     "labels": ["dev", "personal"]
-//   },
-//   {
-//     "id": 2,
-//     "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
-//     "read": false,
-//     "starred": false,
-//     "selected": true,
-//     "labels": []
-//   },
-//   {
-//     "id": 3,
-//     "subject": "Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!",
-//     "read": false,
-//     "starred": true,
-//     "labels": ["dev"]
-//   },
-//   {
-//     "id": 4,
-//     "subject": "We need to program the primary TCP hard drive!",
-//     "read": true,
-//     "starred": false,
-//     "selected": true,
-//     "labels": []
-//   },
-//   {
-//     "id": 5,
-//     "subject": "If we override the interface, we can get to the HTTP feed through the virtual EXE interface!",
-//     "read": false,
-//     "starred": false,
-//     "labels": ["personal"]
-//   },
-//   {
-//     "id": 6,
-//     "subject": "We need to back up the wireless GB driver!",
-//     "read": true,
-//     "starred": true,
-//     "labels": []
-//   },
-//   {
-//     "id": 7,
-//     "subject": "We need to index the mobile PCI bus!",
-//     "read": true,
-//     "starred": false,
-//     "labels": ["dev", "personal"]
-//   },
-//   {
-//     "id": 8,
-//     "subject": "If we connect the sensor, we can get to the HDD port through the redundant IB firewall!",
-//     "read": true,
-//     "starred": true,
-//     "labels": ["dev", "gschool"]
-//   }
-// ]
-
-
-
 
 class App extends Component {
   constructor(props) {
@@ -84,9 +20,6 @@ class App extends Component {
       this.setState({data: json._embedded.messages})
     }
 
-    // for (var i = 0; i < data.length; i++) {
-    //   data[i].selected = false
-    //  }
     
     async patchItem(item) {
       const response = await fetch('https://hypermedia-api-server.herokuapp.com/api/messages', {
